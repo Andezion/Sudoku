868616c4abcdf63073d90657dd8ec826a4b2631c
Andezion <245122@edu.p.lodz.pl>
2025-08-02 11:24:52 +0200

    added diagonal sudoku

---
+++ sudoku_diagonal.h
@@ -0,0 +1,259 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class sudoku_diagonal
+{
+    char sudoku[9][9]{};
+public:
+    explicit sudoku_diagonal(char sudoku[9][9])
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                this->sudoku[i][j] = sudoku[i][j];
+            }
+        }
+    }
+
+    void print_sudoku() const
+    {
+        std::cout << std::endl;
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                std::cout << sudoku[i][j] << "  ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+    }
+
+    void init_sudoku()
+    {
+        const std::vector<std::string> rows =
+        {
+            "---613---",
+            "----5----",
+            "1---2---8",
+            "-85---24-",
+            "--7---1--",
+            "-21---87-",
+            "2---7---9",
+            "----8----",
+            "---965---"
+        };
+
+        for (int i = 0; i < 9; ++i)
+        {
+            for (int j = 0; j < 9; ++j)
+            {
+                sudoku[i][j] = rows[i][j];
+            }
+        }
+    }
+
+    bool check_if_correct() const
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                const char value = sudoku[i][j];
+                if (value == '-')
+                {
+                    return false;
+                }
+
+                // Проверка строки
+                for (int k = 0; k < 9; k++)
+                {
+                    if (k != j && sudoku[i][k] == value)
+                    {
+                        return false;
+                    }
+                }
+
+                // Проверка столбца
+                for (int l = 0; l < 9; l++)
+                {
+                    if (l != i && sudoku[l][j] == value)
+                    {
+                        return false;
+                    }
+                }
+
+                // Проверка квадрата 3x3
+                const int square_i = (i / 3) * 3;
+                const int square_j = (j / 3) * 3;
+
+                for (int ii = square_i; ii < square_i + 3; ii++)
+                {
+                    for (int jj = square_j; jj < square_j + 3; jj++)
+                    {
+                        if ((ii != i || jj != j) && sudoku[ii][jj] == value)
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                if (i == j)
+                {
+                    for (int ii = 0; ii < 9; ii++)
+                    {
+                        if (sudoku[ii][ii] == value && i != ii)
+                        {
+                            return false;
+                        }
+                    }
+                }
+
+                if (i + j == 8)
+                {
+                    for (int ii = 0; ii < 9; ii++)
+                    {
+                        if (sudoku[ii][8 - ii] == value && ii != i)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    void create_sudoku()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                sudoku[i][j] = '-';
+            }
+        }
+    }
+
+    void sudoku_solver()
+    {
+        std::vector<int> what_can_stand[9][9];
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                what_can_stand[i][j] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            }
+        }
+
+        int iterations = 0;
+        bool changed = true;
+        while (changed)
+        {
+            changed = false;
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (sudoku[i][j] == '-')
+                    {
+                        continue;
+                    }
+
+                    int number = sudoku[i][j] - '0';
+
+                    // Удалить из строки
+                    for (int jj = 0; jj < 9; jj++)
+                    {
+                        auto & vec = what_can_stand[i][jj];
+                        vec.erase(std::remove(vec.begin(), vec.end(), number), vec.end());
+
+                        if (vec.size() == 1 && sudoku[i][jj] == '-')
+                        {
+                            sudoku[i][jj] = vec.front() + '0';
+                            changed = true;
+                        }
+                    }
+
+                    // Удалить из столбца
+                    for (int ii = 0; ii < 9; ii++)
+                    {
+                        auto & vec = what_can_stand[ii][j];
+                        vec.erase(std::remove(vec.begin(), vec.end(), number), vec.end());
+
+                        if (vec.size() == 1 && sudoku[ii][j] == '-')
+                        {
+                            sudoku[ii][j] = vec.front() + '0';
+                            changed = true;
+                        }
+                    }
+
+                    // Удалить из квадрата 3x3
+                    const int square_i = (i / 3) * 3;
+                    const int square_j = (j / 3) * 3;
+
+                    for (int ii = square_i; ii < square_i + 3; ii++)
+                    {
+                        for (int jj = square_j; jj < square_j + 3; jj++)
+                        {
+                            auto & vec = what_can_stand[ii][jj];
+                            vec.erase(std::remove(vec.begin(), vec.end(), number), vec.end());
+
+                            if (vec.size() == 1 && sudoku[ii][jj] == '-')
+                            {
+                                sudoku[ii][jj] = vec.front() + '0';
+                                changed = true;
+                            }
+                        }
+                    }
+
+                    // Удалить из левой диагонали
+                    if (i == j)
+                    {
+                        for (int ii = 0; ii < 9; ii++)
+                        {
+                            auto & vec = what_can_stand[ii][ii];
+                            vec.erase(std::remove(vec.begin(), vec.end(), number), vec.end());
+
+                            if (vec.size() == 1 && sudoku[ii][ii] == '-')
+                            {
+                                sudoku[ii][ii] = vec.front() + '0';
+                                changed = true;
+                            }
+                        }
+                    }
+
+                    // Удалить из правой диагонали
+                    if (i + j == 8)
+                    {
+                        for (int ii = 0; ii < 9; ii++)
+                        {
+                            auto & vec = what_can_stand[ii][8 - ii];
+                            vec.erase(std::remove(vec.begin(), vec.end(), number), vec.end());
+
+                            if (vec.size() == 1 && sudoku[ii][8 - ii] == '-')
+                            {
+                                sudoku[ii][8 - ii] = vec.front() + '0';
+                                changed = true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            iterations++;
+            std::cout << "This is " + std::to_string(iterations) + " cycle\n";
+            print_sudoku();
+
+            if (check_if_correct())
+            {
+                std::cout << "Sudoku correct!\n";
+            }
+        }
+    }
+};
