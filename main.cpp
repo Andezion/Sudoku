6519ed51590f4329c583c1663bc051a9441f4cba
Andezion <245122@edu.p.lodz.pl>
2025-08-31 19:01:34 +0200

    created diagonal sudoku

--- main.cpp
+++ main.cpp
@@ -10,24 +10,15 @@ int main()
 {
     const sudoku_checker_diagonal classic_checker;
     const sudoku_solver_diagonal solver(classic_checker);
+    sudoku_generator_diagonal generator(classic_checker, solver);
     sudoku_show_diagonal show;
 
-    std::array<std::array<int, 9>, 9> solution = {{
-        {{0, 0, 5, 1, 0, 4, 9, 0, 0}},
-        {{0, 0, 0, 8, 0, 3, 0, 0, 0}},
-        {{0, 0, 3, 0, 2, 0, 8, 0, 0}},
-        {{3, 0, 0, 0, 0, 0, 0, 0, 9}},
-        {{0, 0, 4, 0, 0, 0, 2, 0, 0}},
-        {{1, 0, 0, 0, 0, 0, 0, 0, 8}},
-        {{0, 0, 6, 0, 9, 0, 5, 0, 0}},
-        {{0, 0, 0, 6, 0, 1, 0, 0, 0}},
-        {{0, 0, 9, 5, 0, 2, 6, 0, 0}}
-    }};
+    std::array<std::array<int, 9>, 9> sudoku = generator.generate9(7);
 
-    show.show(solution);
+    show.show(sudoku);
 
-    solver.solve(solution);
-    show.show(solution);
+    solver.solve(sudoku);
+    show.show(sudoku);
 
     return 0;
 }
--- sudoku_generator.h
+++ sudoku_generator.h
@@ -200,9 +200,181 @@ public:
 
 class sudoku_generator_diagonal final : public sudoku_generator
 {
-    int sudoku[9][9]{};
+    std::array<std::array<int, 9>, 9> sudoku{};
 public:
+    explicit sudoku_generator_diagonal(const sudoku_checker& checker, const sudoku_solver& solver)
+        : sudoku_generator(checker, solver) {}
 
+    static bool create_sudoku(int i, int j, std::array<std::array<int, 9>, 9> & sudoku)
+    {
+        if (j == 9)
+        {
+            j = 0;
+            i++;
+        }
+        if (i == 9)
+        {
+            return true;
+        }
+
+        if (sudoku[i][j] != 0)
+        {
+            return create_sudoku(i, j + 1, sudoku);
+        }
+
+        std::vector candidates = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        for (int col = 0; col < 9; col++)
+        {
+            candidates.erase(std::remove(candidates.begin(), candidates.end(), sudoku[i][col]), candidates.end());
+        }
+
+        for (int row = 0; row < 9; row++)
+        {
+            candidates.erase(std::remove(candidates.begin(), candidates.end(), sudoku[row][j]), candidates.end());
+        }
+
+        if (i == j)
+        {
+            for (int d = 0; d < 9; d++)
+            {
+                candidates.erase(std::remove(candidates.begin(), candidates.end(), sudoku[d][d]), candidates.end());
+            }
+        }
+        if (i + j == 8)
+        {
+            for (int d = 0; d < 9; d++)
+            {
+                candidates.erase(std::remove(candidates.begin(), candidates.end(), sudoku[d][8 - d]), candidates.end());
+            }
+        }
+
+        const int start_i = i / 3 * 3;
+        const int start_j = j / 3 * 3;
+        for (int r = start_i; r < start_i + 3; r++)
+        {
+            for (int c = start_j; c < start_j + 3; c++)
+            {
+                candidates.erase(std::remove(candidates.begin(), candidates.end(), sudoku[r][c]), candidates.end());
+            }
+        }
+
+        static std::mt19937 rng(std::random_device{}());
+        std::shuffle(candidates.begin(), candidates.end(), rng);
+
+        for (const int num : candidates)
+        {
+            sudoku[i][j] = num;
+            if (create_sudoku(i, j + 1, sudoku))
+            {
+                return true;
+            }
+            sudoku[i][j] = 0;
+        }
+
+        return false;
+    }
+
+    bool has_unique_solution(std::array<std::array<int, 9>, 9> sudoku) const
+    {
+        int count = 0;
+        count_solutions(sudoku, count, 2);
+        return count == 1;
+    }
+
+    void count_solutions(std::array<std::array<int, 9>, 9> & sudoku, int &count, const int limit) const
+    {
+        if (count >= limit)
+        {
+            return;
+        }
+
+        int row = -1, col = -1;
+        bool found = false;
+
+        for (int i = 0; i < 9 && !found; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                if (sudoku[i][j] == 0)
+                {
+                    row = i;
+                    col = j;
+                    found = true;
+                    break;
+                }
+            }
+        }
+
+        if (!found)
+        {
+            count++;
+            return;
+        }
+
+        for (int num = 1; num <= 9; num++)
+        {
+            if (checker.is_valid_sudoku(sudoku, row, col, num))
+            {
+                sudoku[row][col] = num;
+                count_solutions(sudoku, count, limit);
+                sudoku[row][col] = 0;
+            }
+        }
+    }
+
+    void deleter(const uint8_t level) override
+    {
+        static std::mt19937 rng(std::random_device{}());
+
+        std::vector<std::pair<int,int>> cells;
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                cells.emplace_back(i, j);
+            }
+        }
+
+        std::shuffle(cells.begin(), cells.end(), rng);
+
+        int to_remove = 20 + level * 5;
+        for (auto [i, j] : cells)
+        {
+            if (to_remove <= 0)
+            {
+                break;
+            }
+
+            const int backup = sudoku[i][j];
+            sudoku[i][j] = 0;
+
+            if (!has_unique_solution(sudoku))
+            {
+                sudoku[i][j] = backup;
+            }
+            else
+            {
+                to_remove--;
+            }
+        }
+    }
+
+    std::array<std::array<int, 9>, 9> generate9(const uint8_t level) override
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            for (int j = 0; j < 9; j++)
+            {
+                sudoku[i][j] = 0;
+            }
+        }
+
+        create_sudoku(0, 0, sudoku);
+        deleter(level);
+
+        return sudoku;
+    }
 };
 
 class sudoku_generator_big final : public sudoku_generator
