3e5b06dc1f7ab47889789610116efe09a0490dda
Andezion <245122@edu.p.lodz.pl>
2025-09-01 19:01:44 +0200

    basic flow

--- main.cpp
+++ main.cpp
@@ -1,6 +1,3 @@
-// #include "sudoku_classic.h"
-// #include "sudoku_diagonal.h"
-
 #include "sudoku_generator.h"
 #include "sudoku_show.h"
 #include "sudoku_solver.h"
@@ -8,17 +5,69 @@
 
 int main()
 {
-    const sudoku_checker_diagonal classic_checker;
-    const sudoku_solver_diagonal solver(classic_checker);
-    sudoku_generator_diagonal generator(classic_checker, solver);
-    sudoku_show_diagonal show;
+    int type_sudoku{};
+    std::cout << "Enter type of sudoku: ";
+    std::cin >> type_sudoku;
 
-    std::array<std::array<int, 9>, 9> sudoku = generator.generate9(7);
+    if (type_sudoku == 1)
+    {
+        const sudoku_checker_diagonal checker;
+        const sudoku_solver_diagonal solver(checker);
+        sudoku_generator_diagonal generator(checker, solver);
+        sudoku_show_diagonal show;
 
-    show.show(sudoku);
+        int level{};
+        std::cout << "Enter level of the sudoku: ";
+        std::cin >> level;
 
-    solver.solve(sudoku);
-    show.show(sudoku);
+        std::array<std::array<int, 9>, 9> sudoku = generator.generate9(level);
+
+        show.show(sudoku);
+
+        solver.solve(sudoku);
+        show.show(sudoku);
+    }
+    else if (type_sudoku == 2)
+    {
+        const sudoku_checker_classic checker;
+        const sudoku_solver_classic solver(checker);
+        sudoku_generator_classic generator(checker, solver);
+        sudoku_show_classic show;
+
+        int level{};
+        std::cout << "Enter level of the sudoku: ";
+        std::cin >> level;
+
+        std::array<std::array<int, 9>, 9> sudoku = generator.generate9(level);
+
+        show.show(sudoku);
+
+        solver.solve(sudoku);
+        show.show(sudoku);
+    }
+    else if (type_sudoku == 3)
+    {
+        const sudoku_checker_big checker;
+        const sudoku_solver_big solver(checker);
+        sudoku_generator_big generator(checker, solver);
+        sudoku_show_big show;
+
+        int level{};
+        std::cout << "Enter level of the sudoku: ";
+        std::cin >> level;
+
+        std::array<std::array<int, 16>, 16> sudoku = generator.generate16(level);
+
+        show.show(sudoku);
+
+        solver.solve(sudoku);
+        show.show(sudoku);
+    }
+    else
+    {
+        throw("Error");
+    }
+
 
     return 0;
 }
