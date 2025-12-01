f910c1f2754f454cabfa48b17c79b19c5b1c4bde
Andezion <245122@edu.p.lodz.pl>
2025-12-01 21:35:07 +0100

    added new cool stuff

--- main.cpp
+++ main.cpp
@@ -3,17 +3,53 @@
 #include "sudoku_solver.h"
 #include "raylib.h"
 
-
 int main()
 {
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-    InitWindow(800, 600, "Sudoku with raylib");
-    while (!WindowShouldClose()) {
+    InitWindow(screenWidth, screenHeight, "raylib [shapes] example - basic shapes");
+
+    float rotation = 0.0f;
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        rotation += 0.2f;
+
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Raylib подключен", 20, 20, 30, BLACK);
+
+            ClearBackground(RAYWHITE);
+
+            DrawText("some basic shapes available on raylib", 20, 20, 20, DARKGRAY);
+
+            DrawCircle(screenWidth/5, 120, 35, DARKBLUE);
+            DrawCircleGradient(screenWidth/5, 220, 60, GREEN, SKYBLUE);
+            DrawCircleLines(screenWidth/5, 340, 80, DARKBLUE);
+            DrawEllipse(screenWidth/5, 120, 25, 20, YELLOW);
+            DrawEllipseLines(screenWidth/5, 120, 30, 25, YELLOW);
+
+            DrawRectangle(screenWidth/4*2 - 60, 100, 120, 60, RED);
+            DrawRectangleGradientH(screenWidth/4*2 - 90, 170, 180, 130, MAROON, GOLD);
+            DrawRectangleLines(screenWidth/4*2 - 40, 320, 80, 60, ORANGE);  // NOTE: Uses QUADS internally, not lines
+
+            DrawTriangle((Vector2){ screenWidth/4.0f *3.0f, 80.0f },
+                         (Vector2){ screenWidth/4.0f *3.0f - 60.0f, 150.0f },
+                         (Vector2){ screenWidth/4.0f *3.0f + 60.0f, 150.0f }, VIOLET);
+
+            DrawTriangleLines((Vector2){ screenWidth/4.0f*3.0f, 160.0f },
+                              (Vector2){ screenWidth/4.0f*3.0f - 20.0f, 230.0f },
+                              (Vector2){ screenWidth/4.0f*3.0f + 20.0f, 230.0f }, DARKBLUE);
+
+            DrawPoly((Vector2){ screenWidth/4.0f*3, 330 }, 6, 80, rotation, BROWN);
+            DrawPolyLines((Vector2){ screenWidth/4.0f*3, 330 }, 6, 90, rotation, BROWN);
+            DrawPolyLinesEx((Vector2){ screenWidth/4.0f*3, 330 }, 6, 85, rotation, 6, BEIGE);
+
+            DrawLine(18, 42, screenWidth - 18, 42, BLACK);
         EndDrawing();
     }
+
     CloseWindow();
 
     int type_sudoku{};
