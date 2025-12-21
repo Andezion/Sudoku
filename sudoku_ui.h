594d5140b4320da36c7818fb556531734de9aaf2
Andezion <245122@edu.p.lodz.pl>
2025-12-21 21:04:15 +0100

    added timer structure

--- sudoku_ui.h
+++ sudoku_ui.h
@@ -1,5 +1,47 @@
 #pragma once
 #include <string>
+#include "raylib.h"
+
+struct GameTimer
+{
+    double startTime = 0.0;
+    double pausedTime = 0.0;
+    bool isRunning = false;
+    bool isPaused = false;
+
+    void start()
+    {
+        startTime = GetTime();
+        pausedTime = 0.0;
+        isRunning = true;
+        isPaused = false;
+    }
+
+    void reset()
+    {
+        startTime = 0.0;
+        pausedTime = 0.0;
+        isRunning = false;
+        isPaused = false;
+    }
+
+    double getElapsed() const
+    {
+        if (!isRunning)
+        {
+            return 0.0;
+        }
+        return GetTime() - startTime - pausedTime;
+    }
+
+    void getTimeFormatted(int& hours, int& minutes, int& seconds) const
+    {
+        const int totalSeconds = static_cast<int>(getElapsed());
+        hours = totalSeconds / 3600;
+        minutes = totalSeconds % 3600 / 60;
+        seconds = totalSeconds % 60;
+    }
+};
 
 std::string valueToStr(int v);
 int buttons_handler();
