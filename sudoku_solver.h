3d12811abc8bd0eab177d7300fec818390f55a9d
Andezion <245122@edu.p.lodz.pl>
2025-12-23 13:25:06 +0100

    added statictics

--- sudoku_solver.h
+++ sudoku_solver.h
@@ -1,11 +1,14 @@
 #pragma once
 #include <array>
+#include <chrono>
 #include "sudoku_checker.h"
 
 class sudoku_solver
 {
 protected:
     const sudoku_checker & checker;
+    mutable std::size_t stepsCount = 0;
+    mutable double lastSolveMs = 0.0;
 public:
     explicit sudoku_solver(const sudoku_checker& checker)
         : checker(checker) {}
@@ -23,6 +26,28 @@ public:
         return false;
     };
     virtual ~sudoku_solver() = default;
+
+    template<typename Board>
+    bool solve_with_stats(Board & sudoku) const
+    {
+        stepsCount = 0;
+
+        const auto t0 = std::chrono::steady_clock::now();
+        const bool res = solve(sudoku);
+        const auto t1 = std::chrono::steady_clock::now();
+
+        lastSolveMs = std::chrono::duration<double, std::milli>(t1 - t0).count();
+        return res;
+    }
+
+    std::size_t get_last_steps() const
+    {
+        return stepsCount;
+    }
+    double get_last_time_ms() const
+    {
+        return lastSolveMs;
+    }
 };
 
 class sudoku_solver_classic final : public sudoku_solver
@@ -61,6 +86,7 @@ public:
         {
             if (checker.is_valid_sudoku(sudoku, row, col, num))
             {
+                ++stepsCount;
                 sudoku[row][col] = num;
 
                 if (solve(sudoku))
@@ -112,6 +138,7 @@ public:
         {
             if (checker.is_valid_sudoku(sudoku, row, col, num))
             {
+                ++stepsCount;
                 sudoku[row][col] = num;
 
                 if (solve(sudoku))
@@ -163,6 +190,7 @@ public:
         {
             if (checker.is_valid_sudoku(sudoku, row, col, num))
             {
+                ++stepsCount;
                 sudoku[row][col] = num;
 
                 if (solve(sudoku))
@@ -212,6 +240,7 @@ public:
         {
             if (checker.is_valid_sudoku(sudoku, row, col, num))
             {
+                ++stepsCount;
                 sudoku[row][col] = num;
 
                 if (solve(sudoku))
